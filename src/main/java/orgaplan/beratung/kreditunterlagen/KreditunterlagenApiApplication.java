0dec330069c30d7f5f8c6137fe555af03857d52b
vietphung <harryportervn1993@gmail.com>
2023-08-03 11:11:03 +0200

    Initial commit

---
+++ src/main/java/orgaplan/beratung/kreditunterlagen/KreditunterlagenApiApplication.java
@@ -0,0 +1,13 @@
+package orgaplan.beratung.kreditunterlagen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KreditunterlagenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KreditunterlagenApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/orgaplan/beratung/kreditunterlagen/KreditunterlagenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package orgaplan.beratung.kreditunterlagen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KreditunterlagenApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

aecbf262b30f8509d3883dac1ee72b732f5dc76a
vietphung <harryportervn1993@gmail.com>
2023-08-04 11:51:11 +0200

    Set up

--- src/main/java/orgaplan/beratung/kreditunterlagen/model/User.java
+++ src/main/java/orgaplan/beratung/kreditunterlagen/model/User.java
@@ -2,6 +2,7 @@ package orgaplan.beratung.kreditunterlagen.model;
 
 import org.hibernate.annotations.GenericGenerator;
 
+import javax.validation.constraints.NotNull; // Import the @NotNull annotation
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import lombok.Getter;
@@ -22,33 +23,43 @@ public class User {
     private String companyName;
 
     @Column(name = "first_name", nullable = false)
+    @NotNull
     private String firstName;
 
     @Column(name = "last_name", nullable = false)
+    @NotNull
     private String lastName;
 
     @Column(name = "phone_number", nullable = false)
+    @NotNull
     private String phoneNumber;
 
     @Column(name = "email", nullable = false)
+    @NotNull
     private String email;
 
     @Column(name = "username", nullable = false)
+    @NotNull
     private String username;
 
     @Column(name = "password", nullable = false)
+    @NotNull
     private String password;
 
     @Column(name = "role", nullable = false)
+    @NotNull
     private String role;
 
     @Column(name = "is_company_client", nullable = false)
+    @NotNull
     private Boolean isCompanyClient;
 
     @Column(name = "privacy_policy_accepted", nullable = false)
+    @NotNull
     private Boolean privacyPolicyAccepted;
 
     @Column(name = "terms_and_conditions_accepted", nullable = false)
+    @NotNull
     private Boolean termsAndConditionsAccepted;
 
     @Column(name = "created_at", nullable = false)

8fefcffc6f325c8f471fb45d080e76405f5a5c2a
vietphung <harryportervn1993@gmail.com>
2023-08-07 14:27:16 +0200

    Set up

--- src/main/java/orgaplan/beratung/kreditunterlagen/controller/DocumentController.java
+++ src/main/java/orgaplan/beratung/kreditunterlagen/controller/DocumentController.java
@@ -1,117 +1,45 @@
 package orgaplan.beratung.kreditunterlagen.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import orgaplan.beratung.kreditunterlagen.model.Document;
 import orgaplan.beratung.kreditunterlagen.model.User;
 import orgaplan.beratung.kreditunterlagen.service.DocumentService;
-import orgaplan.beratung.kreditunterlagen.repository.UserRepository;
+import orgaplan.beratung.kreditunterlagen.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import javax.servlet.http.HttpServletRequest;
-import java.io.File;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/documents")
 public class DocumentController {
 
-    private final DocumentService documentService;
-    private final UserRepository userRepository;
-    private final String uploadDir = "uploads/"; // Đường dẫn thư mục uploads
+    private static final Logger logger = LoggerFactory.getLogger(DocumentController.class);
 
     @Autowired
-    public DocumentController(DocumentService documentService, UserRepository userRepository) {
-        this.documentService = documentService;
-        this.userRepository = userRepository;
-    }
+    private DocumentService documentService;
 
-    @PostMapping("/uploadFile")
-    public ResponseEntity<Map<String, Object>> saveDocument(@RequestParam("file") MultipartFile file,
-                                                            @RequestParam("userId") String userId,
-                                                            @RequestParam("docType") String docType) throws IOException {
-        String originalFileName = file.getOriginalFilename();
-        String extension = originalFileName.substring(originalFileName.lastIndexOf("."));
-        String uniqueFileName = UUID.randomUUID().toString() + extension;
+    @Autowired
+    private UserService userService;
 
-        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/documents/downloadFile/")
-                .path(uniqueFileName).toUriString();
-
-        File uploadDirFile = new File(uploadDir);
-        if (!uploadDirFile.exists()) {
-            if (!uploadDirFile.mkdirs()) {
-                throw new IOException("Failed to create upload directory.");
-            }
+    @PostMapping("/upload")
+    public ResponseEntity<?> uploadDocument(@RequestParam("file") MultipartFile file, @RequestParam("type") String documentType, @RequestParam("userId") Long userId) {
+        User user = userService.getUserById(userId.toString());
+        if (user == null) {
+            logger.error("User not found with ID: {}", userId);
+            return new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
         }
 
-        File dest = new File(uploadDir + uniqueFileName);
-        file.transferTo(dest);
-
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new RuntimeException("User not found with id " + userId));
-
-        Document doc = new Document();
-        doc.setUser(user);
-        doc.setDocumentType(docType);
-        doc.setFileName(uniqueFileName); // Lưu tên tệp duy nhất
-        doc.setFilePath(fileDownloadUri);
-
-        documentService.save(doc);
-
-        Map<String, Object> response = new HashMap<>();
-        response.put("message", "Document saved successfully");
-        response.put("fileName", uniqueFileName); // Trả về tên tệp duy nhất
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
-    }
-
-    @GetMapping("/{userId}")
-    public ResponseEntity<List<Document>> getDocumentsByUserId(@PathVariable String userId) {
-        List<Document> docs = documentService.findByUserId(userId);
-        return ResponseEntity.ok().body(docs);
-    }
-
-    @GetMapping("/downloadFile/{fileName}")
-    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
-        File file = new File(uploadDir + fileName);
-        if (!file.exists()) {
-            throw new RuntimeException("File not found: " + fileName);
-        }
-
-        Resource resource;
+        Document document = null;
         try {
-            Path filePath = file.toPath();
-            resource = new UrlResource(filePath.toUri());
-        } catch (MalformedURLException ex) {
-            throw new RuntimeException("File not found: " + fileName, ex);
+            document = documentService.save(file, documentType, user);
+        } catch (Exception e) {
+            logger.error("Error occurred while saving document", e);
+            return new ResponseEntity<>("Error occurred while saving document", HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
-        String contentType = null;
-        try {
-            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
-        } catch (IOException ex) {
-            System.out.println("Could not determine file type.");
-        }
-
-        if (contentType == null) {
-            contentType = "application/octet-stream";
-        }
-
-        return ResponseEntity.ok()
-                .contentType(MediaType.parseMediaType(contentType))
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
-                .body(resource);
+        return new ResponseEntity<>(document, HttpStatus.OK);
     }
 }
--- src/main/java/orgaplan/beratung/kreditunterlagen/service/DocumentService.java
+++ src/main/java/orgaplan/beratung/kreditunterlagen/service/DocumentService.java
@@ -1,27 +1,43 @@
 package orgaplan.beratung.kreditunterlagen.service;
 
+import orgaplan.beratung.kreditunterlagen.model.Document;
+import orgaplan.beratung.kreditunterlagen.model.User;
+import orgaplan.beratung.kreditunterlagen.repository.DocumentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import orgaplan.beratung.kreditunterlagen.model.Document;
-import orgaplan.beratung.kreditunterlagen.repository.DocumentRepository;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
 
 @Service
 public class DocumentService {
 
-    private final DocumentRepository documentRepository;
+    private static String UPLOADED_FOLDER = "uploads";
 
     @Autowired
-    public DocumentService(DocumentRepository documentRepository) {
-        this.documentRepository = documentRepository;
-    }
+    private DocumentRepository documentRepository;
 
-    public Document save(Document document) {
+    public Document save(MultipartFile file, String documentType, User user) {
+        Document document = new Document();
+        document.setDocumentType(documentType);
+        document.setUser(user);
+
+        // Save the file locally
+        Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+        try {
+            Files.write(path, file.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Could not store the file. Error: " + e.getMessage());
+        }
+
+        document.setFileName(file.getOriginalFilename());
+        document.setFilePath(path.toString());
+
         return documentRepository.save(document);
-    }
-
-    public List<Document> findByUserId(String userId) {
-        return documentRepository.findByUserId(userId);
     }
 }

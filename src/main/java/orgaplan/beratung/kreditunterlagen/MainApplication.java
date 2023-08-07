4da3dc97cc742a896f1376ceb54c3811f7e71356
vietphung <harryportervn1993@gmail.com>
2023-08-07 12:27:01 +0200

    Set up

--- src/main/java/orgaplan/beratung/kreditunterlagen/MainApplication.java
+++ src/main/java/orgaplan/beratung/kreditunterlagen/MainApplication.java
@@ -27,14 +27,21 @@ public class MainApplication {
 	public ApplicationRunner applicationRunner() {
 		return args -> {
 			String serverUrl = "http://localhost:8080";
-			Map<RequestMappingInfo, HandlerMethod> handlerMethods = requestMappingHandlerMapping.getHandlerMethods();
-			for (RequestMappingInfo info : handlerMethods.keySet()) {
-				Set<String> patterns = info.getPatternsCondition().getPatterns();
-				patterns.forEach(pattern -> {
-					System.out.println(serverUrl + pattern);
-				});
-				System.out.println(info.getMethodsCondition().getMethods());
-			}
+
+			printEndpointsForController("UserController", serverUrl);
+			printEndpointsForController("DocumentController", serverUrl);
 		};
+	}
+
+	private void printEndpointsForController(String controllerName, String serverUrl) {
+		System.out.println(controllerName + " Endpoints:");
+		Map<RequestMappingInfo, HandlerMethod> controllerMethods = requestMappingHandlerMapping.getHandlerMethods();
+		for (RequestMappingInfo info : controllerMethods.keySet()) {
+			Set<String> patterns = info.getPatternsCondition().getPatterns();
+			patterns.forEach(pattern -> {
+				System.out.println(serverUrl + pattern);
+			});
+			System.out.println(info.getMethodsCondition().getMethods());
+		}
 	}
 }

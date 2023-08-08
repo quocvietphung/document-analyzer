31b8bfd3bede614d8aafd918ac37bcefb72992f9
vietphung <harryportervn1993@gmail.com>
2023-08-08 17:41:01 +0200

    Set up

--- src/main/java/orgaplan/beratung/kreditunterlagen/controller/UserController.java
+++ src/main/java/orgaplan/beratung/kreditunterlagen/controller/UserController.java
@@ -5,7 +5,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import orgaplan.beratung.kreditunterlagen.model.User;
-import orgaplan.beratung.kreditunterlagen.response.ApiResponse;
 import orgaplan.beratung.kreditunterlagen.service.UserService;
 
 import java.util.HashMap;
@@ -23,53 +22,59 @@ public class UserController {
     }
 
     @GetMapping("/test")
-    public ResponseEntity<ApiResponse> sayHello() {
-        return new ResponseEntity<>(new ApiResponse(true, "Hello, World!", null), HttpStatus.OK);
+    public ResponseEntity<String> sayHello() {
+        String message = "Hello, World!";
+        return ResponseEntity.ok(message);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<ApiResponse> login(@RequestBody Map<String, String> loginRequest) {
+    public ResponseEntity<Map<String, Object>> login(@RequestBody Map<String, String> loginRequest) {
         String username = loginRequest.get("username");
         String password = loginRequest.get("password");
 
         User user = userService.login(username, password);
         if (user != null) {
-            Map<String, Object> data = new HashMap<>();
-            data.put("user_id", user.getId());
-            data.put("username", user.getUsername());
-            return new ResponseEntity<>(new ApiResponse(true, "Login successful", data), HttpStatus.OK);
+            Map<String, Object> response = new HashMap<>();
+            response.put("user_id", user.getId());
+            response.put("username", user.getUsername());
+            response.put("message", "Login successful");
+            return ResponseEntity.ok(response);
         } else {
-            return new ResponseEntity<>(new ApiResponse(false, "Invalid username or password", null), HttpStatus.UNAUTHORIZED);
+            Map<String, Object> response = new HashMap<>();
+            response.put("message", "Invalid username or password");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
         }
     }
 
     @PostMapping("/createUser")
-    public ResponseEntity<ApiResponse> createUser(@RequestBody User user) {
+    public ResponseEntity<Map<String, Object>> createUser(@RequestBody User user) {
         User newUser = userService.createUser(user);
-        return new ResponseEntity<>(new ApiResponse(true, "User created successfully", newUser), HttpStatus.CREATED);
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", "User created successfully");
+        response.put("user", newUser);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @GetMapping
-    public ResponseEntity<ApiResponse> getUsers() {
-        List<User> users = userService.getUsers();
-        return new ResponseEntity<>(new ApiResponse(true, "Users retrieved successfully", users), HttpStatus.OK);
+    public List<User> getUsers() {
+        return userService.getUsers();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ApiResponse> getUserById(@PathVariable String id) {
+    public ResponseEntity<User> getUserById(@PathVariable String id) {
         User user = userService.getUserById(id);
-        return new ResponseEntity<>(new ApiResponse(true, "User retrieved successfully", user), HttpStatus.OK);
+        return ResponseEntity.ok(user);
     }
 
     @PutMapping("/editUser/{id}")
-    public ResponseEntity<ApiResponse> editUser(@PathVariable String id, @RequestBody User updatedUserFields) {
+    public ResponseEntity<User> editUser(@PathVariable String id, @RequestBody User updatedUserFields) {
         User updatedUser = userService.editUser(id, updatedUserFields);
-        return new ResponseEntity<>(new ApiResponse(true, "User updated successfully", updatedUser), HttpStatus.OK);
+        return ResponseEntity.ok(updatedUser);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<ApiResponse> deleteUser(@PathVariable String id) {
+    public ResponseEntity<Void> deleteUser(@PathVariable String id) {
         userService.deleteUser(id);
-        return new ResponseEntity<>(new ApiResponse(true, "User deleted successfully", null), HttpStatus.NO_CONTENT);
+        return ResponseEntity.noContent().build();
     }
 }

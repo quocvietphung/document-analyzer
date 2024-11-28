0d4b1dade833ca157b88353360d43167a6edfdf8
vietphung <harryportervn1993@gmail.com>
2024-11-28 23:11:39 +0100

    Refactor

--- src/main/resources/db/migration/V2__Create_documents_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE documents
-(
-    id CHAR(36) PRIMARY KEY,
-    user_id CHAR(36) NOT NULL,
-    document_type VARCHAR(255),
-    file_name VARCHAR(255),
-    file_path VARCHAR(255),
-    FOREIGN KEY (user_id) REFERENCES users (id)
-);

72b0966a914e6cb8be7785b684c017680af5b1c5
vietphung <harryportervn1993@gmail.com>
2025-09-13 18:46:52 +0200

    Refactor app configuration to use withFetch for improved HTTP client functionality

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,13 +1,13 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http'; // ✅ thêm withFetch
 import { FormsModule } from '@angular/forms';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withFetch()),
     importProvidersFrom(FormsModule)
   ]
 };

18fa14965fa78146bc95dd4f0b6d5de3046a21d3
vietphung <harryportervn1993@gmail.com>
2025-09-13 17:45:06 +0200

    Add initial components for document management: DocumentList, DocumentUpload, Dashboard, Login, and Register

---
+++ frontend/src/app/pages/dashboard/document-list/document-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-document-list',
+  imports: [],
+  templateUrl: './document-list.component.html',
+  styleUrl: './document-list.component.scss'
+})
+export class DocumentListComponent {
+
+}
---
+++ frontend/src/app/pages/dashboard/document-upload/document-upload.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-document-upload',
+  imports: [],
+  templateUrl: './document-upload.component.html',
+  styleUrl: './document-upload.component.scss'
+})
+export class DocumentUploadComponent {
+
+}
